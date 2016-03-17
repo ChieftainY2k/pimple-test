f9d1d7ac0c8925691af795d8934d5e6c89f2f069
ChieftainY2k <ChieftainY2k@gmail.com>
2016-03-18 00:05:54 +0100

    initial commit

---
+++ test2.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Pimple test
+ */
+//use Pimple\Container;
+
+include(__DIR__ . "/vendor/autoload.php");
+date_default_timezone_set("Europe/Warsaw");
+
+
+class DateFormatter
+{
+    private $dateFormat = "Y-m-d";
+
+    function __construct()
+    {
+        echo "\n *** ".__CLASS__." created\n";
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * @param string $dateFormat
+     */
+    public function setDateFormat($dateFormat)
+    {
+        $this->dateFormat = $dateFormat;
+    }
+
+    /**
+     * @param $timestamp
+     * @return bool|string
+     */
+    public function getFormattedDate($timestamp)
+    {
+        return date($this->getDateFormat(), $timestamp);
+    }
+
+    function __clone()
+    {
+        echo "\n *** ".__CLASS__." cloned\n";
+    }
+
+
+}
+
+
+class CurrentDate
+{
+    /**
+     * @var DateFormatter
+     */
+    private $dateFormatter = null;
+
+    function __construct()
+    {
+        echo "\n *** ".__CLASS__." created\n";
+    }
+
+    /**
+     * @return DateFormatter
+     */
+    public function getDateFormatter()
+    {
+        return $this->dateFormatter;
+    }
+
+    /**
+     * @param null $dateFormatter
+     */
+    public function setDateFormatter($dateFormatter)
+    {
+        $this->dateFormatter = $dateFormatter;
+    }
+
+    /**
+     *
+     */
+    public function getCurrentDate()
+    {
+        return $this->getDateFormatter()->getFormattedDate(time());
+    }
+
+    function __clone()
+    {
+        echo "\n *** ".__CLASS__." cloned\n";
+    }
+}
+
+//$c = new CurrentDate();
+//$c2 = clone $c;
+//exit;
+
+$container = new Pimple\Container();
+
+$container['random_func'] = $container->protect(function () {
+    return rand();
+});
+
+$container['date_format'] = "Y-m-d H:i";
+
+$container['date_formatter'] = function ($c) {
+    $dateFormatter = new DateFormatter();
+    $dateFormatter->setDateFormat($c['date_format']);
+    return $dateFormatter;
+};
+
+$container['current_date_getter'] = function($c) {
+    $currentDate = new CurrentDate();
+    $currentDate->setDateFormatter($c['date_formatter']);
+    return $currentDate;
+};
+
+
+//print_r($container);
+
+$currentDateGetter = $container['current_date_getter'];
+var_dump($currentDateGetter->getCurrentDate());
+
+$currentDateGetter = $container['current_date_getter'];
+var_dump($currentDateGetter->getCurrentDate());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
